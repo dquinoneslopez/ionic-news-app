7df7ae7f3feb722a89f2671be4962be93f4783df
Daniel Quiñones López <dquinoneslopez@gmail.com>
2021-06-15 14:45:16 +0200

    Added Components module. Added NewsComponent and NewComponent.

---
+++ src/app/components/new/new.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../../interfaces/interfaces';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.scss'],
+})
+export class NewComponent implements OnInit {
+  @Input() new: Article;
+  @Input() i: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
