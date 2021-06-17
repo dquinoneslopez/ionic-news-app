6f3e1f07ece72d28caca93a9f553d0dcdd86e4eb
Daniel Quiñones López <dquinoneslopez@gmail.com>
2021-06-17 10:47:28 +0200

    Show news by category.

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NewsService } from '../../services/news.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -17,6 +18,7 @@ export class Tab2Page implements OnInit {
     'sports',
     'technology',
   ];
+  news: Article[] = [];
 
   @ViewChild(IonSegment, { static: true }) segment: IonSegment;
 
@@ -24,10 +26,17 @@ export class Tab2Page implements OnInit {
 
   ngOnInit() {
     this.segment.value = this.categories[0];
-    this.newsService
-      .getTopHeadlinesByCategory(this.categories[0])
-      .subscribe((resp) => {
-        console.log(resp);
-      });
+    this.loadNews(this.segment.value);
+  }
+
+  changeCategory(event) {
+    this.news = [];
+    this.loadNews(event.detail.value);
+  }
+
+  loadNews(cat: string) {
+    this.newsService.getTopHeadlinesByCategory(cat).subscribe((resp) => {
+      this.news = [...resp.articles];
+    });
   }
 }
