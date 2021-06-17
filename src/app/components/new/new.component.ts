0f785043a72e6d96c99104a2052f465639812589
Daniel Quiñones López <dquinoneslopez@gmail.com>
2021-06-17 12:39:16 +0200

    Action sheet buttons.

--- src/app/components/new/new.component.ts
+++ src/app/components/new/new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 import { Article } from '../../interfaces/interfaces';
 
@@ -12,11 +13,50 @@ export class NewComponent implements OnInit {
   @Input() new: Article;
   @Input() i: number;
 
-  constructor(private iab: InAppBrowser) {}
+  constructor(
+    private iab: InAppBrowser,
+    private actionSheetCtrl: ActionSheetController
+  ) {}
 
   ngOnInit() {}
 
   openNew() {
     const browser = this.iab.create(this.new.url, '_system');
+  }
+
+  async launchMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Share',
+          icon: 'share',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Share clicked');
+          },
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Favorite clicked');
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          cssClass: 'action-dark',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 }
